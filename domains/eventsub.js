55cf90020b874c1cdaee859dd0b5464a32a8df64
J-D6543 <77695304+J-D6543@users.noreply.github.com>
2021-09-04 23:14:15 -0400

    changed live/offline notifs to match changes to commands

--- domains/eventsub.js
+++ domains/eventsub.js
@@ -36,8 +36,9 @@ app.post('/follow', async (req, res) => {
                 return res.status(200).send('we gucci')
             }
             con.query(
-                `SELECT * FROM follownotifications 
-                WHERE channelUID = ${mysql.escape(req.body.event.broadcaster_user_id)}`, 
+                `SELECT * 
+                FROM follownotifications 
+                WHERE channelUID = ?`, [req.body.event.broadcaster_user_id], 
                 (err, result) => {
                     if (err) throw err;
                     client.say(result[0].channel, `${result[0].message}`.replace("$follower", req.body.event.user_name))
@@ -62,32 +63,36 @@ app.post('/live', async (req, res) => {
         } else if (req.headers['twitch-eventsub-message-type'] === 'webhook_callback_verification' && req.headers['twitch-eventsub-subscription-type'] === 'stream.offline') {
             return res.status(200).send(req.body.challenge)
         } else if (req.headers['twitch-eventsub-message-type'] === 'notification' && req.headers['twitch-eventsub-subscription-type'] === 'stream.online') {
+            const [liveInfo] = await con.promise().query(
+                `SELECT isLive, messageLive, peopleLive, notifications 
+                FROM live 
+                WHERE channelUID = ?`, [req.body.event.broadcaster_user_id]
+            )
+            if (liveInfo[0].isLive === "true") {
+                return res.status(200).send('bruh?')
+            }
             con.query(
                 `UPDATE live 
                 SET isLive = 'true' 
-                WHERE channelUID = ${mysql.escape(req.body.event.broadcaster_user_id)}`
+                WHERE channelUID = ?`, [req.body.event.broadcaster_user_id]
             )
-            const [result] = await con.promise().query(
-                `SELECT messageLive, peopleLive, notifications FROM live 
-                WHERE channelUID = ${mysql.escape(req.body.event.broadcaster_user_id)}`
-            )
-            if (result[0].notifications === 'yes') {
-                if (result[0].peopleLive === null || result[0].peopleLive === '') {
-                    client.say(req.body.event.broadcaster_user_login, result[0].messageLive)
+            if (liveInfo[0].notifications === 'yes') {
+                if (liveInfo[0].peopleLive === null || liveInfo[0].peopleLive === '') {
+                    client.say(req.body.event.broadcaster_user_login, liveInfo[0].messageLive)
                     return res.status(200).send('we gucci')
                 }
-                const peopleArray = result[0].peopleLive.split(' ');
+                const peopleArray = liveInfo[0].peopleLive.split(' ').slice(0, -1)
                 const splitMessage = {};
                 let n = 0
                 for (i in peopleArray) {
                     if (splitMessage.hasOwnProperty(n) === false) {
-                        splitMessage[n] = result[0].messageLive + ' ' + peopleArray[i]
+                        splitMessage[n] = liveInfo[0].messageLive + ' ' + peopleArray[i]
                     } else {
                         if (splitMessage[n].length < 400) {
                             splitMessage[n] = splitMessage[n] + ' ' + peopleArray[i]
                         } else {
                             n = n + 1
-                            splitMessage[n] = result[0].messageLive + ' ' + peopleArray[i]
+                            splitMessage[n] = liveInfo[0].messageLive + ' ' + peopleArray[i]
                         }
                     }
                 }
@@ -99,30 +104,37 @@ app.post('/live', async (req, res) => {
             }
             return res.status(200).send('we gucci')
         } else if (req.headers['twitch-eventsub-message-type'] === 'notification' && req.headers['twitch-eventsub-subscription-type'] === 'stream.offline') {
+            const [offlineInfo] = await con.promise().query(
+                `SELECT isLive, messageOffline, peopleOffline, notifications 
+                FROM live 
+                WHERE channelUID = ?`, [req.body.event.broadcaster_user_id]
+            )
+            if (offlineInfo[0].isLive === "false") {
+                return res.status(200).send('bruh?')
+            }
             con.query(
                 `UPDATE live 
                 SET isLive = 'false' 
-                WHERE channelUID = ${mysql.escape(req.body.event.broadcaster_user_id)}`
+                WHERE channelUID = ?`, [req.body.event.broadcaster_user_id]
             )
-            const [result] = await con.promise().query(`SELECT * FROM live WHERE channelUID = ${mysql.escape(req.body.event.broadcaster_user_id)}`)
-            if (result[0].notifications === 'yes') {
-                if (result[0].peopleOffline === null || result[0].peopleOffline === '') {
-                    client.say(req.body.event.broadcaster_user_login, result[0].messageOffline)
+            if (offlineInfo[0].notifications === 'yes') {
+                if (offlineInfo[0].peopleOffline === null || offlineInfo[0].peopleOffline === '') {
+                    client.say(req.body.event.broadcaster_user_login, offlineInfo[0].messageOffline)
                     return res.status(200).send('we gucci')
                 }
-                const peopleArray = result[0].peopleOffline.split(' ');
+                const peopleArray = offlineInfo[0].peopleOffline.split(' ').slice(0, -1)
                 const splitMessage = {};
                 let n = 0
                 for (i in peopleArray) {
                     if (splitMessage.hasOwnProperty(n) === false) {
-                        splitMessage[n] = result[0].messageOffline + ' ' + peopleArray[i]
+                        splitMessage[n] = offlineInfo[0].messageOffline + ' ' + peopleArray[i]
                     }
                     else {
                         if (splitMessage[n].length < 400) {
                             splitMessage[n] = splitMessage[n] + ' ' + peopleArray[i]
                         } else {
                             n = n + 1
-                            splitMessage[n] = result[0].messageOffline + ' ' + peopleArray[i]
+                            splitMessage[n] = offlineInfo[0].messageOffline + ' ' + peopleArray[i]
                         }
                     }
                 }
